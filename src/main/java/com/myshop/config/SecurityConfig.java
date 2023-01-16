9e63de7dd8209f9741d8843cfe639ba714549f17
github eunbiro <ebee91@naver.com>
2023-01-16 15:57:46 +0900

    0116memberform

--- src/main/java/com/myshop/config/SecurityConfig.java
+++ src/main/java/com/myshop/config/SecurityConfig.java
@@ -17,8 +17,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		
 	}
 	
-	@Bean
-	public PasswordEncoder passwordEncoder() { // 비밀번호 암호화를 위해서 사용하는 반(Bean)
+	@Bean		// @Configuration 얘랑 주로 같이 사용하고 싱글톤이 보장된다.
+	public PasswordEncoder passwordEncoder() { // 비밀번호 암호화를 위해서 사용하는 빈(Bean)
 		return new BCryptPasswordEncoder();
 	}
 	
--- src/main/java/com/myshop/service/MemberService.java
+++ src/main/java/com/myshop/service/MemberService.java
@@ -9,14 +9,15 @@ import com.myshop.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 
 @Service					// service 클래스의 역할
-@Transactional
-@RequiredArgsConstructor
+@Transactional				// 서비스클래스에서 로직을 처리하다 오류 발생 시 로직을 수행하기 이전으로 돌려줌(DB의 rollback 같은 느낌) 
+@RequiredArgsConstructor	// 의존성 주입 시 final @NotNull 키워드가 붙을 때 @Autiwire 대신 사용하며 생성자를 자동생성 
 public class MemberService {
 
 	private final MemberRepository memberRepository;	// 의존성 주입
 	
 	public Member saveMember(Member member) {
 		
+		vaildateDuplicateMember(member);
 		return memberRepository.save(member);			// member 테이브렝 insert
 	}
 	
